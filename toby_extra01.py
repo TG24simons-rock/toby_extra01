874645732e9b19ffdd42685fbad41c6c2fcaa909
TG24simons-rock <tgomberg24@simons-rock.edu>
2024-10-06 14:27:05 -0400

    Add files via upload

---
+++ toby_extra01.py
@@ -0,0 +1,60 @@
+import random
+#i feel like i could have had the random number generation be seperate from the main game in a lot more graceful of a way but whatever
+def numgamewnewnum():
+    global secretnum
+    secretnum = random.randint(2, 999) #you said between 1 and 1000 so i assumed that meant not inclusive
+    global guesses
+    guesses = 0
+    global invalids
+    invalids = 0
+    exit()
+    numgame()
+def numgame():
+    global userguess
+    userguess= input("guess a number between 1 and 1000? ")
+    if userguess.isdigit() == False:
+        userguess = 1
+    global userguessi
+    userguessi = int(userguess)
+    if guesses == 10:
+        pagainl = input("uh oh, no more guesses left. do you want to play again? y/n ?")
+        if pagainl == "y" or "Y":
+            numgamewnewnum()
+        else: 
+            exit()
+            #i know its not that graceful to just close everything if one declines to play  again but what else coulkd i do? also i was to lazy to really acount for not yes no answers.         
+    if userguessi < 1000 and userguessi > 1:
+        Answer()
+    else:
+        print("that's not a possible answer!") 
+        global invalids
+        invalids += 1
+        if invalids == 10:
+            print("you literally wasted all your guesses on answers that couldn't possibly be correct, i give up")
+            exit()
+        else:
+             print("whatever, guess again...")
+             numgame()
+def Answer():
+    global guesses
+    guesses += 1
+    if userguessi < secretnum:
+        print("think higher, guess again")
+        numgame()
+    if userguessi > secretnum:
+        print("think lower, guess again")
+        numgame()
+    if userguessi == secretnum:
+        print("DING DING DING weeeee Haaaaavvvvee a wiiiinnnnerrr!!!!!!!!")
+        pagainw = input("anyways do you want to play again y/n ?")
+        if pagainw == "y" or "Y":
+            numgamewnewnum()
+        else: 
+            exit()
+numgamewnewnum()
+
+
+
+
+   
+        
